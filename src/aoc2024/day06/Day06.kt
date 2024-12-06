8b4ddb9f29a6cca278316b571b62ff96f9946782
Emanuele Albero <emanuele.albero@geniussports.com>
2024-12-06 13:20:12 +0100

    Day06 - parallel execution

--- src/aoc2024/day06/Day06.kt
+++ src/aoc2024/day06/Day06.kt
@@ -1,6 +1,7 @@
 package aoc2024.day06
 
 import common.*
+import java.util.concurrent.atomic.AtomicInteger
 
 fun main() {
 
@@ -30,64 +31,66 @@ fun main() {
         val grid = CharGrid2D(input){ it -> it}
         val startPosition = grid.find('^') ?: return 0
         val loopsMoves: MutableSet<Item<Char>> = mutableSetOf()
-        var loops = 0
+        var loops: AtomicInteger = AtomicInteger(0)
         val bounceIdentified = '@'
 
         visitedPosition
             .filterNot { it == startPosition }
+            .parallelStream()
             .forEach { (x, y) ->
-            grid.setValue(x, y, '#')
-            grid.setPosition(x,y)
-            val obstacleInteractionMoves =
-                grid
-                    .getNeighbors(listOf(Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT))
-                    .filter { it.value != '#'}
-                    .flatMap { value ->
-                        listOf(
-                            Item<Char>(value.x, value.y, Direction.Inverted(value.direction), value.value),
-                            Item<Char>(value.x, value.y, Direction.Inverted(value.direction), bounceIdentified),
-                        ) }
+                val localGrid = CharGrid2D(input){ it -> it}
+                localGrid.setValue(x, y, '#')
+                localGrid.setPosition(x,y)
+                val obstacleInteractionMoves =
+                    localGrid
+                        .getNeighbors(listOf(Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT))
+                        .filter { it.value != '#'}
+                        .flatMap { value ->
+                            listOf(
+                                Item<Char>(value.x, value.y, Direction.Inverted(value.direction), value.value),
+                                Item<Char>(value.x, value.y, Direction.Inverted(value.direction), bounceIdentified),
+                            ) }
 
-            val moves: MutableList<Item<Char>> = mutableListOf()
+                val moves: MutableList<Item<Char>> = mutableListOf()
 
-            grid.setPosition(startPosition.first, startPosition.second)
-            var direction = Direction.UP
-            var loopFound = false
-            while (grid.canMove(direction)) {
-                var nextPosition = grid.getNeighbors(listOf(direction))[0]
-                while (nextPosition.value == '#') {
-                    direction = Direction.rotateRight(direction)
-                    moves.add(Item(
-                        grid.getCurrentPosition().first,
-                        grid.getCurrentPosition().second,
-                        direction,
-                        bounceIdentified,
-                    ))
-                    nextPosition = grid.getNeighbors(listOf(direction))[0]
+                localGrid.setPosition(startPosition.first, startPosition.second)
+                var direction = Direction.UP
+                var loopFound = false
+                while (localGrid.canMove(direction)) {
+                    var nextPosition = localGrid.getNeighbors(listOf(direction))[0]
+                    while (nextPosition.value == '#') {
+                        direction = Direction.rotateRight(direction)
+                        moves.add(Item(
+                            localGrid.getCurrentPosition().first,
+                            localGrid.getCurrentPosition().second,
+                            direction,
+                            bounceIdentified,
+                        ))
+                        nextPosition = localGrid.getNeighbors(listOf(direction))[0]
+                    }
+
+                    if (nextPosition in loopsMoves) {
+                        loopFound = true
+                        break
+                    }
+
+                    val previousOccurrence = moves.indexOf(nextPosition)
+                    if ( previousOccurrence >= 0) {
+                        /*if (obstacleInteractionMoves.none{ moves.indexOf(it) >= previousOccurrence }) {
+                            loopsMoves.addAll(moves.subList(previousOccurrence, moves.size-1))
+                        }*/
+                        loopFound = true
+                        break
+                    }
+                    moves.add(nextPosition)
+                    localGrid.move(direction)
                 }
-
-                if (nextPosition in loopsMoves) {
-                    loopFound = true
-                    break
+                if (loopFound) {
+                    loops.getAndIncrement()
                 }
-
-                val previousOccurrence = moves.indexOf(nextPosition)
-                if ( previousOccurrence >= 0) {
-                    /*if (obstacleInteractionMoves.none{ moves.indexOf(it) >= previousOccurrence }) {
-                        loopsMoves.addAll(moves.subList(previousOccurrence, moves.size-1))
-                    }*/
-                    loopFound = true
-                    break
-                }
-                moves.add(nextPosition)
-                grid.move(direction)
+                localGrid.setValue(x, y, '.')
             }
-            if (loopFound) {
-                loops++
-            }
-            grid.setValue(x, y, '.')
-        }
-        return loops
+            return loops.get()
     }
 
     // test if implementation meets criteria from the description, like:
