ac5a14ac4d8d3bfe3cdaf60266885bdc83aa12cf
Emanuele Albero <emanuele.albero@geniussports.com>
2024-12-07 11:17:58 +0100

    Day06 - new strategy

--- src/aoc2024/day06/Day06.kt
+++ src/aoc2024/day06/Day06.kt
@@ -1,6 +1,7 @@
 package aoc2024.day06
 
 import common.*
+import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
 fun main() {
@@ -27,41 +28,89 @@ fun main() {
         return visitedPosition.size
     }
 
-    fun part2(input: List<String>): Int {
-        val grid = CharGrid2D(input){ it -> it}
-        val startPosition = grid.find('^') ?: return 0
-        var loops: AtomicInteger = AtomicInteger(0)
+    fun part2(input: List<String>, visitedPosition: Set<Pair<Int, Int>>): Int {
+        val grid = input.map { it.toCharArray() }.toTypedArray()
+        val width = grid[0].size
+        val height = grid.size
+        var startPosition: Pair<Int, Int> = Pair(0, 0)
 
+        val rows = mutableMapOf<Int, TreeSet<Int>>()
+        val cols = mutableMapOf<Int, TreeSet<Int>>()
+
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                if (grid[y][x] == '#') {
+                    rows.computeIfAbsent(y) { TreeSet() }.add(x)
+                    cols.computeIfAbsent(x) { TreeSet() }.add(y)
+                }
+                if (grid[y][x] == '^') {
+                    startPosition = Pair(x, y)
+                }
+            }
+        }
+
+        fun nextMovement(x: Int, y: Int, direction: Direction): Triple<Int, Int, Direction>? {
+            return when (direction) {
+                Direction.UP -> {
+                    val columnObs = cols[x] ?: return null
+                    val nextStep = columnObs.lower(y) ?: return null
+                    Triple(x, nextStep + 1, Direction.RIGHT)
+                }
+
+                Direction.DOWN -> {
+                    val columnObs = cols[x] ?: return null
+                    val nextStep = columnObs.higher(y) ?: return null
+                    Triple(x, nextStep - 1, Direction.LEFT)
+                }
+
+                Direction.LEFT -> {
+                    val rowObs = rows[y] ?: return null
+                    val nextStep = rowObs.lower(x) ?: return null
+                    Triple(nextStep + 1, y, Direction.UP)
+                }
+
+                Direction.RIGHT -> {
+                    val rowObs = rows[y] ?: return null
+                    val nextStep = rowObs.higher(x) ?: return null
+                    Triple(nextStep - 1, y, Direction.DOWN)
+                }
+
+                else -> return null
+            }
+        }
+
+        var loops = 0
         visitedPosition
             .filterNot { it == startPosition }
-            .parallelStream()
-            .forEach { (x, y) ->
-                val localGrid = CharGrid2D(input){ it -> it}
-                val moves: MutableSet<Item<Char>> = mutableSetOf()
-                localGrid.setValue(x, y, '#')
+            .forEach { (ox, oy) ->
+                rows.computeIfAbsent(oy) { TreeSet() }
+                cols.computeIfAbsent(ox) { TreeSet() }
 
-                var direction = Direction.UP
-                var isLoop: Boolean = false
-                localGrid.setPosition(startPosition.first, startPosition.second)
-                while (localGrid.canMove(direction)) {
-                    var nextPosition = localGrid.getNeighbors(listOf(direction))[0]
-                    while (nextPosition.value == '#') {
-                        direction = Direction.rotateRight(direction)
-                        nextPosition = localGrid.getNeighbors(listOf(direction))[0]
-                    }
+                rows[oy]!!.add(ox)
+                cols[ox]!!.add(oy)
 
-                    if ( moves.contains(nextPosition) ) {
-                        isLoop = true
+                val storedMovements = mutableSetOf<Triple<Int, Int, Direction>>()
+                var nextMovementState: Triple<Int, Int, Direction>? = Triple(startPosition.first, startPosition.second, Direction.UP)
+
+                var loopFound = false
+                while (true) {
+                    if (nextMovementState == null) break
+                    if (nextMovementState in storedMovements) {
+                        loopFound = true
                         break
                     }
-
-                    moves.add(nextPosition)
-                    localGrid.move(direction)
+                    storedMovements.add(nextMovementState)
+                    nextMovementState = nextMovement(nextMovementState.first, nextMovementState.second, nextMovementState.third)
                 }
-                if (isLoop) loops.incrementAndGet()
-                localGrid.setValue(x, y, '.')
+
+                if (loopFound) {
+                    loops++
+                }
+
+                rows[oy]!!.remove(ox)
+                cols[ox]!!.remove(oy)
             }
-            return loops.get()
+        return loops
     }
 
     // test if implementation meets criteria from the description, like:
@@ -70,7 +119,7 @@ fun main() {
     part1Result.println()
     check(part1Result == 41)
 
-    val part2Result = part2(testInput)
+    val part2Result = part2(testInput, visitedPosition)
     part2Result.println()
     check(part2Result == 6)
 
@@ -80,6 +129,6 @@ fun main() {
     }
 
     benchmarkTime("part2") {
-        part2(input)
+        part2(input, visitedPosition)
     }
 }
