7d8bd2b2f86c9401cb4d25eef79beebc554f0cb4
Emanuele Albero <emanuele.albero@geniussports.com>
2024-12-15 18:26:06 +0100

    Day15 - update

--- src/aoc2024/day15/Day15.kt
+++ src/aoc2024/day15/Day15.kt
@@ -3,173 +3,170 @@ package aoc2024.day15
 import common.*
 import common.Direction.*
 
-fun main() {
+const val EMPTY = '.'
+const val BOX = 'O'
+const val BOX_LEFT = '['
+const val BOX_RIGHT = ']'
+const val WALL = '#'
+const val ROBOT = '@'
 
-    val boxesValues = intArrayOf(1,3,4)
+data class Cords(var x: Int, var y: Int)
 
-    data class Cords(var x: Int, var y: Int)
+fun Direction.toCords(): Cords = when (this) {
+    LEFT -> Cords(-1,0)
+    RIGHT -> Cords(1,0)
+    UP -> Cords(0,-1)
+    DOWN -> Cords(0,1)
+    else -> Cords(0,0)
+}
 
-    fun Direction.toCords(): Cords = when (this) {
-        LEFT -> Cords(-1,0)
-        RIGHT -> Cords(1,0)
-        UP -> Cords(0,-1)
-        DOWN -> Cords(0,1)
-        else -> Cords(0,0)
+class Robot(val position: Cords) {
+    private val boxesValues = arrayOf(BOX,BOX_LEFT,BOX_RIGHT)
+
+    fun push(grid: CharGrid2D, direction: Direction) {
+        val move = direction.toCords()
+        val nextElement = grid.getElementAt(position.x + move.x, position.y + move.y)
+
+        when (nextElement) {
+            WALL -> return
+            EMPTY -> {
+                position.x += move.x
+                position.y += move.y
+                return
+            }
+            else -> {
+                when (direction) {
+                    LEFT, RIGHT -> pushHorizontally(grid, direction, move)
+                    else -> pushVertically(grid, direction, move)
+                }
+            }
+        }
     }
 
-    class Robot(val position: Cords) {
-        fun push(grid: NumberGrid2D, direction: Direction) {
-            val move = direction.toCords()
-            val nextElement = grid.getElementAt(position.x + move.x, position.y + move.y)
-            if (nextElement == -1) {
-                return
-            }
-            if (nextElement == 0) {
-                position.x += move.x
-                position.y += move.y
-                grid.setValue(position.x, position.y, 0)
-                return
-            }
+    private fun pushVertically(grid: CharGrid2D, direction: Direction, move: Cords) {
+        var canBeMoved = false
+        val boxToMove: MutableSet<Item<Char>> = mutableSetOf()
+        var toBeMoved: MutableSet<Cords> = mutableSetOf()
 
-            when (direction) {
-                LEFT, RIGHT -> pushHorizontally(grid, direction, move)
-                else -> pushVertically(grid, direction, move)
+        val newPosition = Cords(position.x + move.x, position.y + move.y)
+        addBoxToStack(toBeMoved, grid, newPosition)
+        toBeMoved.forEach { boxToMove.add(Item(it.x, it.y, null, grid.getElementAt(it.x, it.y)!!)) }
+        while (!toBeMoved.any { grid.getElementAt(it.x, it.y+move.y)!!  == WALL}){
+
+            if (toBeMoved.all { grid.getElementAt(it.x, it.y+move.y)!!  == EMPTY}){
+                canBeMoved = true
+                break
             }
-        }
+            val toBeMoved2 = mutableSetOf<Cords>()
 
-        private fun pushVertically(grid: NumberGrid2D, direction: Direction, move: Cords) {
-            var canBeMoved = false
-            val boxToMove: MutableSet<Item<Int>> = mutableSetOf()
-            var toBeMoved: MutableSet<Cords> = mutableSetOf()
+            toBeMoved
+                .filter {
+                    grid.getElementAt(it.x, it.y+move.y)!!  in boxesValues
+                }
+                .forEach {
+                    addBoxToStack(toBeMoved2, grid, Cords(it.x, it.y+move.y))
+                }
 
-            val newPosition = Cords(position.x + move.x, position.y + move.y)
-            addBox(toBeMoved, grid, newPosition)
+            toBeMoved = toBeMoved2
             toBeMoved.forEach { boxToMove.add(Item(it.x, it.y, null, grid.getElementAt(it.x, it.y)!!)) }
-            while (!toBeMoved.any { grid.getElementAt(it.x, it.y+move.y)!!  == -1}){
-
-                if (toBeMoved.all { grid.getElementAt(it.x, it.y+move.y)!!  == 0}){
-                    canBeMoved = true
-                    break
-                }
-                val toBeMoved2 = mutableSetOf<Cords>()
-
-                toBeMoved
-                    .filter {
-                        grid.getElementAt(it.x, it.y+move.y)!!  in boxesValues
-                    }
-                    .forEach {
-                        addBox(toBeMoved2, grid, Cords(it.x, it.y+move.y))
-                    }
-
-                toBeMoved = toBeMoved2
-                toBeMoved.forEach { boxToMove.add(Item(it.x, it.y, null, grid.getElementAt(it.x, it.y)!!)) }
-            }
-
-            if (canBeMoved) {
-                position.x += move.x
-                position.y += move.y
-
-                when (direction) {
-                    UP -> {
-                        boxToMove
-                            .sortedBy { it.y }
-                            .forEach {
-                                grid.setValue(it.x, it.y - 1, it.value)
-                                grid.setValue(it.x, it.y, 0)
-                            }
-                    }
-                    else -> {
-                        boxToMove
-                            .sortedByDescending { it.y }
-                            .forEach {
-                                grid.setValue(it.x, it.y + 1, it.value)
-                                grid.setValue(it.x, it.y, 0)
-                            }
-                    }
-                }
-                grid.setValue(position.x, position.y, 0)
-
-            }
         }
 
-        private fun pushHorizontally(grid: NumberGrid2D, direction: Direction, move: Cords) {
-            var stack = 0
-
-            var newPosition = Cords(position.x + move.x, position.y + move.y)
-            var nextElement = grid.getElementAt(newPosition.x, newPosition.y) ?: 0
-            while (nextElement in boxesValues) {
-                while (grid.getElementAt(newPosition.x, newPosition.y)!! in boxesValues) {
-                    stack++
-                    newPosition = Cords(position.x + (move.x * stack), position.y + (move.y * stack))
-                }
-                stack -= 1
-                nextElement = grid.getElementAt(newPosition.x, newPosition.y) ?: 0
-            }
-            if (nextElement == -1) {
-                return
-            }
-
+        if (canBeMoved) {
             position.x += move.x
             position.y += move.y
 
             when (direction) {
-                LEFT -> {
-                    for (x in stack downTo 1) {
-                        grid.setValue(
-                            position.x - x, position.y,
-                            grid.getElementAt(position.x - x + 1, position.y)!!
-                        )
-                    }
+                UP -> {
+                    boxToMove
+                        .sortedBy { it.y }
+                        .forEach {
+                            grid.setValue(it.x, it.y - 1, it.value)
+                            grid.setValue(it.x, it.y, EMPTY)
+                        }
                 }
                 else -> {
-                    for (x in stack downTo 1) {
-                        grid.setValue(
-                            position.x + x, position.y,
-                            grid.getElementAt(position.x + x - 1, position.y)!!
-                        )
-                    }
+                    boxToMove
+                        .sortedByDescending { it.y }
+                        .forEach {
+                            grid.setValue(it.x, it.y + 1, it.value)
+                            grid.setValue(it.x, it.y, EMPTY)
+                        }
                 }
             }
-            grid.setValue(position.x, position.y, 0)
-        }
-
-        private fun addBox(toBeMoved: MutableSet<Cords>, grid: NumberGrid2D, box: Cords) {
-            toBeMoved.add(box)
-            if (grid.getElementAt(box.x, box.y) == 3) {
-                toBeMoved.add(Cords(box.x+1, box.y))
-            }
-            if (grid.getElementAt(box.x, box.y) == 4) {
-                toBeMoved.add(Cords(box.x-1, box.y))
-            }
+            grid.setValue(position.x, position.y, EMPTY)
 
         }
     }
 
-    fun printMap(grid: NumberGrid2D, robot: Robot) {
+    private fun pushHorizontally(grid: CharGrid2D, direction: Direction, move: Cords) {
+        var stack = 0
+        var newPosition = Cords(position.x + move.x, position.y + move.y)
+        var nextElement = grid.getElementAt(newPosition.x, newPosition.y) ?: EMPTY
+        while (nextElement in boxesValues) {
+            while (grid.getElementAt(newPosition.x, newPosition.y)!! in boxesValues) {
+                stack++
+                newPosition = Cords(position.x + (move.x * stack), position.y + (move.y * stack))
+            }
+            stack -= 1
+            nextElement = grid.getElementAt(newPosition.x, newPosition.y) ?: EMPTY
+        }
+        if (nextElement == WALL) {
+            return
+        }
+
+        position.x += move.x
+        position.y += move.y
+
+        when (direction) {
+            LEFT -> {
+                for (x in stack downTo 1) {
+                    grid.setValue(
+                        position.x - x, position.y,
+                        grid.getElementAt(position.x - x + 1, position.y)!!
+                    )
+                }
+            }
+            else -> {
+                for (x in stack downTo 1) {
+                    grid.setValue(
+                        position.x + x, position.y,
+                        grid.getElementAt(position.x + x - 1, position.y)!!
+                    )
+                }
+            }
+        }
+        grid.setValue(position.x, position.y, EMPTY)
+    }
+
+    private fun addBoxToStack(toBeMoved: MutableSet<Cords>, grid: CharGrid2D, box: Cords) {
+        toBeMoved.add(box)
+        if (grid.getElementAt(box.x, box.y) == BOX_LEFT) {
+            toBeMoved.add(Cords(box.x+1, box.y))
+        }
+        if (grid.getElementAt(box.x, box.y) == BOX_RIGHT) {
+            toBeMoved.add(Cords(box.x-1, box.y))
+        }
+    }
+}
+
+fun main() {
+    fun printMap(grid: CharGrid2D, robot: Robot) {
         val builder = StringBuilder()
-        grid.setValue(robot.position.x, robot.position.y, 2)
+        grid.setValue(robot.position.x, robot.position.y, ROBOT)
         for (y in 0 until grid.height) {
             for (x in 0 until grid.width) {
                 builder.append(
-                    when (grid.getElementAt(x, y)) {
-                        -1 -> '#'
-                        0 -> '.'
-                        1 -> 'O'
-                        2 -> '@'
-                        3 -> '['
-                        4 -> ']'
-                        else -> ' '
-                    }
+                    grid.getElementAt(x, y)
                 )
             }
             builder.append('\n')
         }
         println(builder.toString())
-        grid.setValue(robot.position.x, robot.position.y, 0)
+        grid.setValue(robot.position.x, robot.position.y, EMPTY)
     }
 
-    fun calculateGridScore(grid: NumberGrid2D): Long {
-        return grid.findAll { it == 1 || it == 3 }.sumOf {
+    fun calculateGridScore(grid: CharGrid2D): Long {
+        return grid.findAll { it == BOX || it == BOX_LEFT }.sumOf {
             (100L*it.second) + it.first
         }
     }
@@ -177,17 +174,8 @@ fun main() {
     fun part1(input: String): Long {
         val gridData = input.substringBefore("\r\n\r\n").split("\r\n")
         val operationsData = input.substringAfter("\r\n\r\n")
-        val grid =
-            NumberGrid2D(
-                gridData
-            ) {
-                when (it) {
-                    '#' -> -1
-                    'O' -> 1
-                    '@' -> 2
-                    else -> 0
-                }
-            }
+
+        val grid = CharGrid2D(gridData)
         val operations: List<Direction> =
             operationsData
                 .mapNotNull {
@@ -200,17 +188,10 @@ fun main() {
                     }
                 }
 
-        val robot = grid.find(2)!!.let { Robot( Cords(it.first, it.second)) }
-        grid.setValue(robot.position.x, robot.position.y, 0)
+        val robot = grid.find(ROBOT)!!.let { Robot( Cords(it.first, it.second)) }
+        grid.setValue(robot.position.x, robot.position.y, EMPTY)
 
-        //printMap(grid, robot)
-        operations.forEachIndexed { _, it ->
-            robot.push(grid, it)
-            /*if (index < operations.size - 1)
-                operations[index+1].println()
-            printMap(grid, robot)
-            readln()*/
-        }
+        operations.forEach { robot.push(grid, it) }
 
         printMap(grid, robot)
         return calculateGridScore(grid)
@@ -231,19 +212,8 @@ fun main() {
                 }.joinToString("")
             }
         val operationsData = input.substringAfter("\r\n\r\n")
-        val grid =
-            NumberGrid2D(
-                gridData
-            ) {
-                when (it) {
-                    '#' -> -1
-                    'O' -> 1
-                    '@' -> 2
-                    '[' -> 3
-                    ']' -> 4
-                    else -> 0
-                }
-            }
+
+        val grid = CharGrid2D(gridData)
         val operations: List<Direction> =
             operationsData
                 .mapNotNull {
@@ -256,13 +226,10 @@ fun main() {
                     }
                 }
 
-        val robot = grid.find(2)!!.let { Robot( Cords(it.first, it.second)) }
-        grid.setValue(robot.position.x, robot.position.y, 0)
+        val robot = grid.find('@')!!.let { Robot( Cords(it.first, it.second)) }
+        grid.setValue(robot.position.x, robot.position.y, EMPTY)
 
-        operations.forEachIndexed { _, it ->
-            robot.push(grid, it)
-        }
-
+        operations.forEach { robot.push(grid, it) }
         printMap(grid, robot)
 
         return calculateGridScore(grid)
